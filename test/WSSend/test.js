ef882b0a3de23ea2c1fd55f0e0d9ee05a27b70f8
cjus <Carlos.Justiniano@gmail.com>
2016-10-16 10:27:31 -0400

    Updated to fwsp-hydra 0.10.2 and fwsp-umf-message 0.4.0. Updated tests.

--- test/WSSend/test.js
+++ test/WSSend/test.js
@@ -5,12 +5,12 @@ const UMFMessage = require('fwsp-umf-message');
 let ws = new WebSocket('http://localhost:5353');
 
 ws.on('open', () => {
-  let umf = UMFMessage({
+  let umf = UMFMessage.createMessage({
     'to': 'hydra-router:[GET]/v1/router/list/nodes',
     'from': 'client:/',
     'body': {}
   });
-  ws.send(umf.toJSON());
+  ws.send(JSON.stringify(umf));
 });
 
 ws.on('message', (data, flags) => {
