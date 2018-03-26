b63f55b53fcc29f2f188e01fece71a963dce4f4e
cjus <Carlos.Justiniano@gmail.com>
2018-03-26 17:46:44 -0400

    Update docker test stack

--- docker/up.sh
+++ docker/up.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+source ~/.bash_profile
 export STACK_NAME='hr'
 export HOST_IP=`echo "show State:/Network/Global/IPv4" | scutil | grep PrimaryInterface | awk '{print $3}' | xargs ifconfig | grep inet | grep -v inet6 | awk '{print $2}'`
 echo "Binding ${STACK_NAME} to ${HOST_IP}"
 
 sudo -- sh -c -e "echo '$HOST_IP\thost' >> /etc/hosts"
 
-mkdir -p ~/data/${STACK_NAME}
-sudo STACK_NAME=${STACK_NAME} HOST_IP=${HOST_IP} HYDRA_REDIS_URL=redis://${HOST_IP}:6379/15 docker stack deploy --compose-file stack-compose.yml --with-registry-auth ${STACK_NAME}
+mkdir -p ~/data/${STACK_NAME} ~/data/${STACK_NAME}/redis ~/data/${STACK_NAME}/elasticsearch ~/data/${STACK_NAME}/mongo
+
+sudo STACK_NAME=${STACK_NAME} HOST_IP=${HOST_IP} docker stack deploy --compose-file base-stack-compose.yml --with-registry-auth ${STACK_NAME}
+sleep 15
+sudo STACK_NAME=${STACK_NAME} HOST_IP=${HOST_IP} docker stack deploy --compose-file stack-compose.yml --with-registry-auth ${STACK_NAME}
