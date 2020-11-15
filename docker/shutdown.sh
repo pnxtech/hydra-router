bc17ab9da92374b2ca06649ed955de0cceb0b193
cjus <Carlos.Justiniano@gmail.com>
2020-11-15 10:34:43 -0700

    shutdown script cleanup

--- docker/shutdown.sh
+++ docker/shutdown.sh
@@ -2,6 +2,3 @@ export STACK_NAME='hr-cluster'
 sudo sed '/[[:space:]]host/d' /etc/hosts > hosts.bak
 sudo mv hosts.bak /etc/hosts
 docker stack rm ${STACK_NAME}
-
-#echo "\nClosing services"
-#./countdown.sh 20
