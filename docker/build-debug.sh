7d39b80c1ef7f9ec7c48b4a1f34e129db9475458
cjus <Carlos.Justiniano@gmail.com>
2021-11-01 20:18:29 -0600

    Update to use Node 16 and build of arm64 and amd64 docker images.

--- docker/build-debug.sh
+++ docker/build-debug.sh
@@ -1,6 +1,5 @@
 VERSION_TAG=$(<VERSION)
 cd ..
 rm -rf node_modules
-docker buildx build --platform=linux/amd64 -f Dockerfile.debug --no-cache=true --load -t hydra-router:$VERSION_TAG .
-
+docker build -f Dockerfile.debug --no-cache=true --load -t hydra-router:$VERSION_TAG .
 
--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,20 @@
 VERSION_TAG=$(<VERSION)
+SERVICE_NAME=hydra-router
+SERVICE_HOST=pnxtech
+
 cd ..
 rm -rf node_modules
-docker buildx build --platform=linux/amd64 --load --no-cache -t hydra-router:$VERSION_TAG .
- 
+
+docker build --no-cache -t $SERVICE_HOST/$SERVICE_NAME:$VERSION_TAG-amd64 --build-arg ARCH=amd64 .
+docker build --no-cache -t $SERVICE_HOST/$SERVICE_NAME:$VERSION_TAG-arm64 --build-arg ARCH=arm64 .
+
+docker push $SERVICE_HOST/$SERVICE_NAME:$VERSION_TAG-amd64
+docker push $SERVICE_HOST/$SERVICE_NAME:$VERSION_TAG-arm64
+
+docker manifest create \
+$SERVICE_HOST/$SERVICE_NAME:$VERSION_TAG \
+--amend $SERVICE_HOST/$SERVICE_NAME:$VERSION_TAG-amd64 \
+--amend $SERVICE_HOST/$SERVICE_NAME:$VERSION_TAG-arm64 
+
+docker manifest push $SERVICE_HOST/$SERVICE_NAME:$VERSION_TAG
+
