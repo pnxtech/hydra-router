01ba42e60db2931c100fafa80fcdfd33e86f4411
cjus <Carlos.Justiniano@gmail.com>
2021-11-16 14:49:27 -0700

    support for passing cache-control header

--- docker/build-debug.sh
+++ docker/build-debug.sh
@@ -1,5 +1,6 @@
 VERSION_TAG=$(<VERSION)
+SERVICE_NAME=hydra-router
 cd ..
 rm -rf node_modules
-docker build -f Dockerfile.debug --no-cache=true --load -t hydra-router:$VERSION_TAG .
+docker build -f Dockerfile.debug --no-cache=true -t $SERVICE_NAME:$VERSION_TAG .
 
